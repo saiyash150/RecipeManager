d4ff0f07d664eccc1553bf1d9ac6fb510763b902
saiyash150 <saipandu150@gmail.com>
2025-03-09 15:35:30 +0530

    removed unecessary code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import RecipeSubmissionForm from "./components/RecipeSubmissionForm";
 import RecipeList from "./components/RecipeList";
-import { getRecipes } from "./api/api";
-import DragAndDrop from "./components/DragAndDrop";
 
 import "./styles/App.module.css";
 function App() {
   return (
     <div>
-      <h1 style={{ textAlign: "center" ,paddingTop:10}}>Recipe Management Application</h1>
+      <h1 style={{ textAlign: "center", paddingTop: 10 }}>
+        Recipe Management Application
+      </h1>
 
       <RecipeList />
     </div>
